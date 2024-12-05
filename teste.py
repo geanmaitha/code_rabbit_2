5c2555277a20887d4b7c95c5041ca0418a8b9771
geanmaitha <gean.cruz@maitha.com.br>
2024-12-05 18:19:10 -0300

    feat: Subindo dados de gean/HP-2100

--- teste.py
+++ teste.py
@@ -1,11 +1,11 @@
 import numpy as np
 import pandas as pd
 
-gean = [12, 12, 3, 34, 2, 3.14]
+gean = [12, 12, 3, 34, 2, 3.14
 
 
 for i in gean:
-    print(f'Segue os números kkk: {i}')\n
+    print(f'Segue os números kkk: {i}')\n\n
 
 
 print("Código Finalizado! ")
